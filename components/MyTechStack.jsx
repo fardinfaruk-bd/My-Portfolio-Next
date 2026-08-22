232088553692422862650900808ebb3a4c942f3b
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-08-22 17:32:25 +0600

    Edit the Tech Stack Images and Add some new stack skill image

--- components/MyTechStack.jsx
+++ components/MyTechStack.jsx
@@ -11,21 +11,25 @@ const MyTechStack = () => {
             <div className="reveal-item">
               <Marquee 
                 pauseOnHover={true} 
-                speed={80} 
+                speed={100} 
                 gradient={true} 
                 gradientColor="var(--background)"
                 gradientWidth={100}
                 className='overflow-hidden'
-              >
+
+                >
                 {[
-                  { src: "https://i.ibb.co.com/LDTKLd2X/HTML.png", alt: "Html" },
-                  { src: "https://i.ibb.co.com/tTg4FqMJ/CSS.png", alt: "CSS" },
-                  { src: "https://i.ibb.co.com/rR2wWPqm/Js.png", alt: "JavaScript" },
-                  { src: "https://i.ibb.co.com/zHQkGk0j/Next-js.png", alt: "Next js" },
-                  { src: "https://i.ibb.co.com/vxKZLc92/Mongo-Db-2.png", alt: "Mongo DB" },
+                  { src: "https://i.ibb.co.com/3YpsCdvJ/HTML.png", alt: "Html" },
+                  { src: "https://i.ibb.co.com/6RhMdYqc/CSS.png", alt: "CSS" },
+                  { src: "https://i.ibb.co.com/9msLphCT/Js.png", alt: "JavaScript" },
+                  { src: "https://i.ibb.co.com/mF2whX51/Type-Script.png", alt: "Type Script" },
+                  { src: "https://i.ibb.co.com/1GRZb7Jq/Next.png", alt: "Next js" },
+                  { src: "https://i.ibb.co.com/gL4QscCD/Node.png", alt: "Node js" },
+                  { src: "https://i.ibb.co.com/Q3zt1Cjb/Express.png", alt: "Express" },
+                  { src: "https://i.ibb.co.com/1tyJzNVB/Mongo-DB.png", alt: "Mongo DB" },
                 ].map((tech, i) => (
                   <div key={i} className="mx-12 grayscale-0 hover:grayscale-100 transition-all duration-500 opacity-50 hover:opacity-100 transform hover:scale-110">
-                    <Image src={tech.src} alt={tech.alt} width={120} height={120} className="object-contain" />
+                    <Image src={tech.src} alt={tech.alt} width={200} height={200} className="object-contain" />
                   </div>
                 ))}
               </Marquee>
